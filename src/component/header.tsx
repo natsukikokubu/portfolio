4463ae77a4771c972e7b201717001565252513e7
natsukikokubu <natsukikokubu3@gmail.com>
2024-08-06 19:05:12 +0900

    header修正

---
+++ src/component/header.tsx
@@ -0,0 +1,20 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>natsuki's portfolio</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#top">Top</a>
+          </li>
+          <li>
+            <a href="#works">Works</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
