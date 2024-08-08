8955bcee234448264c5518fdacb049c0a0d957fa
natsukikokubu <natsukikokubu3@gmail.com>
2024-08-08 20:29:40 +0900

    add scroll

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,25 @@
+import { Link as ScrollLink } from "react-scroll";
+
 export const Header = () => {
   return (
-    <header className="flex justify-between px-4 h-20 w-screen bg-blue-600">
-      <h1 className="text-2xl align-middle">natsuki's portfolio</h1>
+    <header className="flex justify-between px-10 h-20 w-screen bg-gray-100">
+      <h1 className="text-2xl flex items-center">natsuki's portfolio</h1>
       <nav>
-        <ul className="flex gap-4 items-center h-full">
+        <ul className="flex gap-10 items-center h-full">
           <li>
-            <a href="#top">Top</a>
+            <ScrollLink to="top" smooth duration={300}>
+              Top
+            </ScrollLink>
           </li>
           <li>
-            <a href="#works">Works</a>
+            <ScrollLink to="top" smooth duration={300}>
+              Works
+            </ScrollLink>
           </li>
           <li>
-            <a href="#contact">Contact</a>
+            <ScrollLink to="top" smooth duration={300}>
+              Contact
+            </ScrollLink>
           </li>
         </ul>
       </nav>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,15 @@ export default function Home() {
   return (
     <main className="font-body">
       <Header />
+      <div className="h-80 bg-slate-400">
+        <h1 id="top">About me</h1>
+      </div>
+      <div className="h-80 bg-slate-500">
+        <h1 id="works">Works</h1>
+      </div>
+      <div className="h-80 bg-slate-600">
+        <h1 id="contact">Contact</h1>
+      </div>
     </main>
   );
 }
