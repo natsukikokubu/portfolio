30030f1080e559f97dd6c0cf91847043c88c62f9
natsukikokubu <natsukikokubu3@gmail.com>
2024-08-15 20:56:45 +0900

    修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import { Header } from "@/components";
-import { WorkCard } from "@/components";
+import { Header, WorkCard } from "@/components";
 
 export default function Home() {
   return (
